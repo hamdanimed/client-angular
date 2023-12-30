dda1556e92aa8397d89cdfcf99d4a301209bd29f
hamdanimed <hamdanimee@gmail.com>
2023-12-30 15:26:22 +0100

    start of home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+
+  constructor(private router:Router){}
+
+  price=1000000;
+  transfers=[
+    {"timestamp":1703945878,"sender":"Client 1","reciever":"Client 2","amount":2000},
+    {"timestamp":1703945878,"sender":"Client 1","reciever":"Client 2","amount":2000},
+    {"timestamp":1703945878,"sender":"Client 1","reciever":"Client 2","amount":2000},
+    {"timestamp":1703945878,"sender":"Client 1","reciever":"Client 2","amount":2000},
+  ]
 
 }
